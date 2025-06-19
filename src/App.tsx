d7108ced8c056607df204567845677290c6df7c5
black-adm <mattheuscontato17@gmail.com>
2025-06-19 13:47:49 -0300

    project base

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen w-full flex justify-center items-center">
+      <h1 className="text-4xl font-bold">Conectar UI</h1>
+    </div>
+  )
+}
+
+export default App
